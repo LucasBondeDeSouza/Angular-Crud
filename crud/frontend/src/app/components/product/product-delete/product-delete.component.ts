3530b4924caed2c8bfacb5321768ed45194fd7de
LucasBondeDeSouza <111398229+LucasBondeDeSouza@users.noreply.github.com>
2023-09-05 16:15:33 -0300

    atualização

--- crud/frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ crud/frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-delete',
@@ -11,20 +12,27 @@ export class ProductDeleteComponent implements OnInit {
 
   product: Product
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService, 
+    private router: Router, 
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-    const id = '1'
+    const id = +this.route.snapshot.paramMap.get('id')
     this.productService.readById(id).subscribe(product => {
       this.product = product
     })
   }
 
   deleteProduct(): void {
-    
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('Produto excluido com Sucesso!')
+      this.router.navigate(['/products'])
+    })
   }
 
   cancel(): void {
-    
+    this.router.navigate(['/products'])
   }
 }
