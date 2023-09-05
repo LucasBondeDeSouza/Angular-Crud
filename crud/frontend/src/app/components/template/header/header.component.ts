8ea026154a4efd77b64585f544361f3a01b31856
LucasBondeDeSouza <111398229+LucasBondeDeSouza@users.noreply.github.com>
2023-09-05 17:06:24 -0300

    atualização

--- crud/frontend/src/app/components/template/header/header.component.ts
+++ crud/frontend/src/app/components/template/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HeaderService } from './header.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor(private headerService: HeaderService) { }
+
+  ngOnInit(): void {
+      
+  }
+
+  get title(): String {
+    return this.headerService.headerData.title
+  }
+
+  get icon(): String {
+    return this.headerService.headerData.icon
+  }
+
+  get routeUrl(): String {
+    return this.headerService.headerData.routeUrl
+  }
 
 }
